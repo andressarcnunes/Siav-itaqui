4c80ddbe2914abacbbe9d9899a44554a26d57ff0
andressarcnunes <andressarcnunes@gmail.com>
2026-09-12 12:36:47 -0300

    Update teste_arduino.py

--- teste_arduino.py
+++ teste_arduino.py
@@ -11,7 +11,7 @@ st.markdown("---")
 porta = st.sidebar.text_input("Porta COM", value="COM3")
 baudrate = 115200
 
-@st.cache(allow_output_mutation=true)
+@st.cache(allow_output_mutation=True)
 def conectar(p, b):
     try:
         ser = serial.Serial(p, b, timeout=1)
